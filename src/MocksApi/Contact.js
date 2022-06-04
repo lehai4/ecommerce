b48b4397a4e3ea5dfe5dff289fab93717e588a21
lehai4 <86140878+lehai4@users.noreply.github.com>
2022-06-04 14:48:22 +0700

    up

---
+++ src/MocksApi/Contact.js
@@ -0,0 +1,5 @@
+export const Contact = [
+  {
+    title: "VISIT US",
+  },
+];
--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Helmet } from "../Common";
+import { Grid, Helmet, Section, SectionBody, SectionTitle } from "../Common";
 const Contact = () => {
   return (
     <>
       <Helmet title="Liên hệ">
-        <div className="contact"></div>
+        <div className="contact">
+          <Section>
+            <SectionTitle>Liên hệ</SectionTitle>
+            <SectionBody>
+              <Grid col={3} mdCol={2} smCol={1} gap={30}></Grid>
+            </SectionBody>
+          </Section>
+        </div>
       </Helmet>
     </>
   );
